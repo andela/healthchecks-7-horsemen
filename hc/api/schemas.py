46019cd25e99cec2972e649a7ffe11eb3e01ae53
David Parseen Maitoyo <david.maitoyo@andela.com>
2017-10-25 11:15:01 +0300

    [Feature[2119336]] added nag_interval and nag_on_status to api models and migrated changes

--- hc/api/schemas.py
+++ hc/api/schemas.py
@@ -4,6 +4,8 @@ check = {
         "tags": {"type": "string"},
         "timeout": {"type": "number", "minimum": 60, "maximum": 604800},
         "grace": {"type": "number", "minimum": 60, "maximum": 604800},
+        "nag_interval": {"type": "number", "minimum": 60, "maximum": 604800},
+        "nag_status_on": {"type":"boolean"},
         "channels": {"type": "string"}
     }
 }
