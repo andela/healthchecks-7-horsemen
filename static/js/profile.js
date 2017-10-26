a5da90f19669aab54f928a61d7cf4c3a2ce9b85d
Chris-Maina <krismaish34@gmail.com>
2017-10-26 16:08:27 +0300

    [Feature #[151948532]] Add change function to enable radio buttons on checking reports allowed checkbox

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
 
-    $(".member-remove").click(function() {
+    $(".member-remove").click(function () {
         var $this = $(this);
 
         $("#rtm-email").text($this.data("email"));
@@ -9,5 +9,18 @@ $(function() {
 
         return false;
     });
+
+
+    $('input[type=radio]').prop('disabled', 'disabled');
+
+    $('input[type=checkbox]').change( function () {
+        if ($(this).is(':checked')){
+            $('input[type=radio]').prop('disabled', false);
+        }
+        else{
+            $('input[type=radio]').prop('disabled', 'disabled');
+        }
+    });
+
 
 });
