e360f905d15283033d4fb5ac1a16ff207ffc680d
Luwum Patrick Alvin <luwyxx@gmail.com>
2017-10-27 13:15:30 +0300

    Add environment variables for SSL and TLS

--- hc/settings.py
+++ hc/settings.py
@@ -142,8 +142,8 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("TLS_ENABLE")
-EMAIL_USE_SSL = os.getenv("SSL_ENABLE")
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 # Slack integration -- override these in local_settings
 SLACK_CLIENT_ID = None
