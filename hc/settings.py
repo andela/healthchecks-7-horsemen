d1e9e4658dfbec44ca67b648659652f967a6ef0c
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-10-27 08:05:43 +0300

    Change from django-ses-backend to django default email backend

--- hc/settings.py
+++ hc/settings.py
@@ -133,7 +133,15 @@ STATICFILES_FINDERS = (
 )
 COMPRESS_OFFLINE = True
 
+# Email configuration with django default backend
 EMAIL_BACKEND = "djmail.backends.default.EmailBackend"
+DJMAIL_REAL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 # Slack integration -- override these in local_settings
 SLACK_CLIENT_ID = None
