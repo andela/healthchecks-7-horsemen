92b85975e66ab2de5fdb3af00d1deef40262cbc6
Chris-Maina <krismaish34@gmail.com>
2017-10-27 10:48:55 +0300

    [Feature #[151948532]] Add on click function to control radio button behavior

--- static/js/profile.js
+++ static/js/profile.js
@@ -11,14 +11,16 @@ $(function () {
     });
 
 
-    $('input[type=radio]').prop('disabled', 'disabled');
-
-    $('input[type=checkbox]').change( function () {
-        if ($(this).is(':checked')){
+    $('.radio').hide();
+    
+    $('input[type=checkbox]').click( function () {
+        if ($('input[type=checkbox]').is(':checked')){
             $('input[type=radio]').prop('disabled', false);
+            $('.radio').show();
         }
         else{
-            $('input[type=radio]').prop('disabled', 'disabled');
+            $('input[type=radio]').prop('disabled', true);
+            $('.radio').hide();
         }
     });
 
