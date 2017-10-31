32955b73c832f4344d95dbcdb91535df06088fbc
Daniel Kipkemei <daniel.kipkemei@andela.com>
2017-10-31 22:08:24 +0300

    [CHORE #151948528] Updating test-check-models

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -13,6 +13,8 @@ class CheckModelTestCase(TestCase):
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
         ### Repeat above test for when check is an empty string
+        check.tags = ""
+        self.assertEquals(check.tags_list(), [])
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -27,6 +29,7 @@ class CheckModelTestCase(TestCase):
 
     def test_paused_check_is_not_in_grace_period(self):
         check = Check()
+        self.assertFalse(check.in_grace_period())
 
         check.status = "up"
         check.last_ping = timezone.now() - timedelta(days=1, minutes=30)
