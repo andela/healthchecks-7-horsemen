de14537b76d4d455eba0d25a9b3e4f1ce3bc0422
Brian Sserumaga <briansserumaga@Your-Girlfriends-bestfriend.local>
2017-11-01 11:03:59 +0300

    [Chore #151948529] Fix tests for login.

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -20,13 +20,18 @@ class LoginTestCase(TestCase):
         assert r.status_code == 302
 
         ### Assert that a user was created
+        user = User.objects.get(email="alice@example.org")
+        checks = Check.objects.all()
+        self.assertEqual(user.email, 'alice@example.org')
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
         ### Assert contents of the email body
+        self.assertIn('To log into healthchecks.io, please open the link below:', mail.outbox[0].body)
 
         ### Assert that check is associated with the new user
+        self.assertEqual(checks[0].user, user)
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
