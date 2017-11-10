d5a3d40394048a3a0f0a6c5d677d0c12e36af955
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-11-10 10:09:23 +0300

    Increase timeout limit in schema and form

--- hc/front/forms.py
+++ hc/front/forms.py
@@ -18,9 +18,9 @@ class NameTagsForm(forms.Form):
 
 
 class TimeoutForm(forms.Form):
-    timeout = forms.IntegerField(min_value=60, max_value=2592000)
-    grace = forms.IntegerField(min_value=60, max_value=2592000)
-    nag = forms.IntegerField(min_value=60, max_value=2592000)
+    timeout = forms.IntegerField(min_value=60, max_value= 29030400)
+    grace = forms.IntegerField(min_value=60, max_value=29030400)
+    nag = forms.IntegerField(min_value=60, max_value=29030400)
     nag_status_on = forms.CharField(required=False)
 
 
